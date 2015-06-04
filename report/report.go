d969fd21fff0c65831da93cdcd826580e9ccfd0c
wott <william.ott@medicustek.com>
2015-06-04 10:43:31 +0800

    Remove Register stuff

--- report/report.go
+++ report/report.go
@@ -1,26 +1,8 @@
 package report
 
-import (
-    "log"
-)
-
-var (
-    reports = make(map[string]Report)
-)
-
 type (
 	// Report defines an interface that reports must implement.
 	Report interface {
 		Read() error // Read contents of Filename
 	}
 )
-
-// Register registers a report with the application.
-func Register(reportType string, report Report) {
-    if _, exists := reports[reportType]; exists {
-        log.Fatalln(reportType, "Report already registered!")
-    }
-
-    log.Println("Register", reportType, "report")
-    reports[reportType] = report
-}
