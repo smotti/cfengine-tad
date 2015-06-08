d0046e18c7a22762460465eeb57f163dfc1d6297
wott <william.ott@medicustek.com>
2015-06-08 16:06:27 +0800

    Add missing flags for reports

--- config/config.go
+++ config/config.go
@@ -12,6 +12,9 @@ var (
 	Channels = flag.String("chan", "#tad", "Channels to join")
 	Ssl      = flag.Bool("ssl", false, "Use SSL/TLS")
 	Listen   = flag.Bool("listenChannel", false, "Listen for command on public channels")
+	HostInfo = flag.String("hostInfo", "./data/va_host_info_report.json", "Path to host info report")
+	Promises = flag.String("promises", "./data/promises.csv", "Path to promises report")
+	Classes  = flag.String("classes", "./data/classes.txt", "Path to classes report")
 )
 
 const (
